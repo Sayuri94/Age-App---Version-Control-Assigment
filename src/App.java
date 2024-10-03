976f84810b3879e3a750bf3d9761902fcc5fe1b7
Sayuri Wijerathna <sayuriwijerathna94@gmail.com>
2024-10-03 11:22:10 +0300

    Checked the second question using different age values

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //2. Start with this code:
+        int age = 8;
+        if (age > 0 && age < 18) {
+            System.out.println ("You are underage");
+        }
+        else {
+            System.out.println ("You are an adult");
+        }
+        System.out.println ("Press space to exit.");
 
 
 
